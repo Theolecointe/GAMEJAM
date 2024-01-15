fe6c0581b4abd7f091edf204db46b58a2787ba8b
DESKTOP-2G8C4VL\ecoleLDLC <etudiant@lecole.com>
2024-01-15 17:30:25 +0100

    WIP

--- Assets/switchView.cs
+++ Assets/switchView.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class switchView : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class switchView : MonoBehaviour
     [SerializeField] GameObject objectToToggle; // L'objet à activer/désactiver
     private KeyCode toggleKey = KeyCode.T;
     // Start is called before the first frame update
+    Vector3 targetPosition; 
+
     void Start()
     {
         
@@ -17,11 +20,15 @@ public class switchView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 targetPosition = transform.position;
 
         if (Keyboard.current[Key.T].isPressed)
         {
+            objectToToggle.transform.position = targetPosition;
+
             this.gameObject.SetActive(false);
             objectToToggle.SetActive(true);
+
         }
     }
 }
