0965558fc71b3bd0b43bfc274a337784faaf7630
DESKTOP-67E422Q\etudiants2 <etudiant@lecole.com>
2024-01-17 09:41:45 +0100

    mm

--- Assets/Récup.cs
+++ Assets/Récup.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Récup : MonoBehaviour
 {
     [SerializeField] int nombreRandom;
     [SerializeField] private bool isLivraison;
+    [SerializeField] private float montantAjoute = 10.0f; 
+    public GestionArgent gestionArgent;
+    public TextMeshProUGUI argentText;
+    private bool dejaTouchee = false;
+
     void OnTriggerEnter(Collider collider)
     {
         Livreur livreur = collider.GetComponent<Livreur>();
@@ -17,6 +24,10 @@ public class Récup : MonoBehaviour
             Debug.Log("Vous avez livré le colis du client !");
             livreur.Drop();
 
+            gestionArgent.AjouterArgent(montantAjoute);
+            UpdateDebugText("Argent" + montantAjoute + "$");
+            dejaTouchee = true;
+
         }
         else if(isLivraison == false && unColis == false)
         {
@@ -24,14 +35,30 @@ public class Récup : MonoBehaviour
             livreur.PickUp();
         }
 
+
       
     }
 
-  
+    private void UpdateDebugText(string v)
+    {
+        throw new NotImplementedException();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
 }
+
+public class GestionArgent
+{
+    internal void AjouterArgent(float v)
+    {
+        throw new NotImplementedException();
+    }
+}
+
